0c5b734d5bbb5f447dbcf09ece4c1993f45af866
Fabian Mueller <fendrin@gmx.de>
2018-05-22 13:52:19 +0200

    Start with sourcing the launcher... Initial commit of love's main file.

---
+++ main.lua
@@ -0,0 +1,9 @@
+----
+-- This file is executed by love after "conf.lua".
+-- Only those two files are ever sourced by the engine,
+-- every other functionallity must be required from here.
+
+-- Calling the "launcher" modules for two reasons,
+-- first we continue coding in moonscript,
+-- second we do not want to clutter the root folder too much
+require"launcher"
