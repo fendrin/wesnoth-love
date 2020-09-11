ffac6454f6534a2fd45b276ec3e284eeb60d9db1
fendrin <fendrin@gmx.de>
2020-09-12 00:09:10 +0200

    This file is now empty. But it needs to stay.
    Love2d does not start without having a file with that name on that place.
    The functionality which was inside has moved to wesnoth/main.moon

--- main.lua
+++ main.lua
@@ -2,11 +2,4 @@
 -- Copyright (C) 2018 by Fabian Mueller <fendrin@gmx.de>
 -- SPDX-License-Identifier: GPL-2.0+
 
--- This file is executed by love after "conf.lua".
--- Only those two files are ever sourced by the engine,
--- every other functionallity must be required from here.
-
--- Calling the "launcher" modules for two reasons,
--- first we continue coding in moonscript,
--- second we do not want to clutter the root folder too much
-require"launcher"
+-- @file without this file love2d will not run!
