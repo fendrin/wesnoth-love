1e26d7ef7530b29290637f91df8f9ccd9b16da09
fendrin <fendrin@gmx.de>
2020-10-25 13:32:19 +0100

    Copy the lpeg c lua lib into the savedir at startup.
    This is needed since we do not want to rely on os specific search paths
    for those libs.
    This is espicially true when run in fuse mode,
    most os can't read from within a zip file when loading libs.

--- conf.lua
+++ conf.lua
@@ -25,11 +25,60 @@ local shared_path = {
 local setRequirePath = require'wesnoth.shared.setRequirePath'
 setRequirePath(shared_path)
 
+-- Copy the lualibs (only lpeg for now) into the savedir.
+-- This makes sure love2d can find it without relying on os specific mechanisms.
+-- This is espacially needed when running in fuse mode, since libs can't be loaded
+-- from within zip files.
+
+local system = require'love.system'
+local osString = system.getOS()
+
+local srcPath, dstPath
+if osString == 'OS X' then
+    srcPath = 'wesnoth/shared/lib/macosx/lpeg.so'
+    dstPath = 'lib/lpeg.so'
+end
+if osString == 'Windows' then
+    srcPath = 'wesnoth/shared/lib/windows/lpeg.dll'
+    dstPath = 'lib/lpeg.dll'
+end
+if osString == 'Linux' then
+    srcPath = 'wesnoth/shared/lib/linux/lpeg.so'
+    dstPath = 'lib/lpeg.so'
+end
+
+local filesystem = require'love.filesystem'
+
+-- todo take from a global
+filesystem.setIdentity('wesnoth')
+if not filesystem.getInfo(dstPath) then
+
+    local success
+    success = filesystem.createDirectory('lib')
+    if not success then
+        print('could not create lib dir')
+    end
+
+    local container = 'data'
+    local data, error = filesystem.read(container, srcPath)
+    if not data then
+        print(error)
+    else
+        local message
+        success, message = filesystem.write(dstPath, data)
+        if not success then
+            print(message)
+        end
+    end
+end
+
 -- First set the lua searchpath of love to our needs, then require moonscript,
 -- which registers its own loader (for files with the .moon extension)
 -- with the luapathes rewriten for moonscript.
+filesystem.setCRequirePath('lib/??')
 require"moonscript"
 
 -- Register the love.conf function after requireing moonscript,
 -- since the function's file could already miss a lua counterpart.
 require"client.conf"
+
